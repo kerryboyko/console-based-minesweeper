9e94f34774f28eb286676236cf0f08b6bf31dd46
Brian Boyko <brian.boyko@gmail.com>
2017-01-04 17:13:28 -0500

    syntax

--- src/repl.js
+++ src/repl.js
@@ -1,17 +1,25 @@
 import readline from 'readline';
-import {playIntro} from './intro'
-import { createMinefield, dig, createIce } from './board';
 
-}
+import {
+  playIntro
+} from './intro'
+
+import {
+  createMinefield,
+  createIce,
+  dig,
+  flagMinefield,
+  checkVictory
+} from './board';
 
 const HELP = [
   'To dig a square, type "dig" and the square name, i.e. "dig C4"',
-  'To mark (or unmark) a square, type "mark" and the square name, i.e., "mark H7"',
+  'To flag (or unflag) a square, type "flag" and the square name, i.e., "flag H7"',
   'To quit, type "quit"',
   'To see these instructions again, type "help"',
 ]
 
-const STANDARD_PROMPT = 'What would you like to do? ("dig", "mark", "quit")'
+const STANDARD_PROMPT = 'What would you like to do? ("dig", "flag", "quit")'
 
 const showHelp = () => {
   HELP.forEach((line) => console.log(line));
@@ -26,11 +34,11 @@ const prompt = (interface, prompt) => new Promise(function(resolve, reject) {
 })
 
 const display = (matrix) => {
-  console.log(" |ABCDEFGHIJ");
-  console.log("-+----------")
-  matrix.map((row, index) => (index.toString() + '|' + row.join('')))
-    .forEach((row) => console.log(row))
-  })
+console.log(" |ABCDEFGHIJ");
+console.log("-+----------")
+matrix.map((row, index) => (index.toString() + '|' + row.join('')))
+  .forEach((row) => console.log(row))
+})
 }
 
 
@@ -47,52 +55,70 @@ const newGame = () => {
 }
 
 class Game {
-  constructor(){
+  constructor() {
     this.flags = 0;
     this.mines = null;
     this.ice = createIce();
     this.interface = readline.createInterface(process.stdin, process.stdout);
   }
 
-  prompt (promptText) { return new Promise((resolve, reject) => {
+  prompt(promptText) => new Promise((resolve, reject) => {
     this.interface.question(promptText, (i) => resolve(i));
-  }
+  })
 
-  start () {
+  start() {
     playIntro().then((() => {
       showHelp();
-      display(this.ice); 
+      display(this.ice);
       this.prompt(STANDARD_PROMPT)
         .then((input) => this.route(input))
     }))
   }
 
-  route (input) {
+  route(input) {
     input = input.toUpper();
     // TODO: Validate input;
-    if(input.substring(0, 4) === "QUIT"){
+
+    if (input.substring(0, 4) === "QUIT") {
       quit(interface);
       return;
-    };
-    if(input.substring(0, 4) === "HELP"){
+
+    } else if (input.substring(0, 4) === "HELP") {
       showHelp();
       this.prompt(STANDARD_PROMPT)
         .then((input) => this.route(input));
-    }
-    if(input.substring(0, 3) === "DIG"){
+
+    } else if (input.substring(0, 3) === "DIG") {
       let digSpot = input.substring(4, 2)
       console.log(`Digging ${digSpot}`)
-      if (this.mines === null){
+      if (this.mines === null) {
         this.mines = createMinefield(digSpot));
       }
       this.ice = dig(this.mines, this.ice, digSpot);
-      if(this.ice === "BOOM"){
+      if (this.ice === "BOOM") {
+        console.log("")
+        display(this.mines);
         this.interface.close();
         process.stdin.destroy();
         newGame();
+        return;
       }
-      // else if this.flags === 20 {
-      display(this.ice);
-
+      this.prompt(STANDARD_PROMPT)
+        .then((input) => this.route(input));
+    } else if (input.substring(0, 4) === "FLAG") {
+      flagMinefield(this.ice, input.substring(5, 2));
+      if (checkVictory(this.mines, this.ice)) {
+        console.log("CONGRATULATIONS!");
+        display(this.mines);
+        this.interface.close();
+        process.stdin.destroy();
+        newGame();
+        return;
+      } else {
+        display(this.ice);
+        this.prompt(STANDARD_PROMPT)
+          .then((input => this.route(input)))
+      }
     }
+  }
 }
